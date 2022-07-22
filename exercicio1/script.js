c9d0c825ca5674cc791895b4c6d20097bd3bb2cd
Joanadayse <daysejoana3@gmail.com>
2022-07-21 21:31:49 -0300

    feito

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+
+// a) Peça ao usuário que insira um número **par**
+let num = Number (prompt ("escolha um numero par"))
+const divisor = 2
+
+let divisão = num / divisor
+let resultado = num % divisor
+
+console.log (resultado)
+console.log (divisor)
+
+// b) Imprima no console **o resto da divisão** desse número por 2.
+
+// c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+      //sim , tudo retorna 0
+// d) O que acontece no console se o usuário inserir um número ímpar? Escreva em um comentário de código
+  // retorna 1
